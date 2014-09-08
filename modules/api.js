8e974c33ac208c6463d823a41906387acf22e743
Your Name <you@example.com>
2014-09-08 09:25:38 -0400

    updates

--- modules/api.js
+++ modules/api.js
@@ -1,5 +1,5 @@
 var request = require('request-json');
-var base_url = "http://192.168.1.165:8080/v1/"
+var base_url = "http://0.0.0.0:8080/v1/"
 var client = request.newClient(base_url);
 exports.request = function (url, data, callback) {
   client.post(base_url+url, data, function (error, response, body) {
--- routes/nodes.js
+++ routes/nodes.js
@@ -10,25 +10,24 @@ router.get('/', function(req, res) {
 router.get('/new', function(req, res){
   res.render('nodes/new', {graph_id: req.query.graph_id})
 })
-var node_create = function (node){
-  api.request('node/create.json', {id: node, label: node}, function (e, r, b){
-    b
+var node_create = function (node, callback){
+  api.request('node/create.json', {label: node}, function (e, r, b){
+    callback(null, b._id)
   })
 }
 router.post('/new', function(req, res){
   var node_ids = [];
   var nodes = req.body.nodes.split(/,|\n/);
-  var funcs = []
-  for (var i = 0;i < nodes.length; i++){
-    funcs.push(node_create(nodes[i]))
-  }
-  api.request('graph/add_node.json', {node_ids: node_ids, graph_id: req.query.graph_id}, function (e, r, b){
-    res.render('nodes/new', {graph_id: req.query.graph_id, node_ids: node_ids, node_ids_count: node_ids.length})
-  })
-
-
-  // while (node_ids.length != req.body.nodes.split(/,|\n/)) {}
+  var graph_id = req.body.graph_id
+  async.map(nodes, node_create, function (err, result) {
+    console.log(node_ids)
+    api.request('graph/add_node.json', {node_ids: node_ids, graph_id: graph_id}, function (e, r, b){
+      console.log(b)
+      res.render('nodes/new', {graph_id: graph_id, node_ids: node_ids, node_ids_count: node_ids.length})
+    })
+  });
 
 
 })
+
 module.exports = router;
